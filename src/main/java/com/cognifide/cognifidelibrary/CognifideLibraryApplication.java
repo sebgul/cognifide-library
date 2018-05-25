f7c2d97983ee9ceb6e3990f3e9cd63c65333d647
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2018-05-25 22:15:42 +0200

    empty app created

---
+++ src/main/java/com/cognifide/cognifidelibrary/CognifideLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.cognifide.cognifidelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognifideLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CognifideLibraryApplication.class, args);
+    }
+}
